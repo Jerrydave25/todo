bdce5feb34f14af5c124e8f3ec84eedb15e6b73d
jerrydave25 <jerrydaveolami@gmail.com>
2022-09-02 23:15:20 +0100

    Javascript design

---
+++ index.js
@@ -0,0 +1,36 @@
+// This is my first javascript App
+console.log('Hello Friend');
+
+alert('Welcome To My First App');
+
+// This is to work on divisible number
+for (var i = 1; i <=10; i++)
+
+	if (i % 3) 
+	  console.log('Fizz');
+	if ( i % 5)
+	  console.log('Buzz');
+	if ((i % 3) && (i % 5))
+	  console.log('FizzBuzz')
+
+	
+
+
+/*// this is using switch
+let x = 10;
+switch (x) {
+  case 0:
+    text = "off";
+    break;
+  case 1:
+    text = "on";
+    break;
+  case 10:
+    text = "comot";
+    break;
+  default:
+    text = "No value found";
+
+}
+console.log(text);
+*/
